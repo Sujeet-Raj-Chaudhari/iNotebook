2209a12213cfd43320e665db544f9266c37f1176
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2022-05-24 19:13:45 +0530

    till video 51

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const { body, validationResult} = require('express-validator');
 var jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-
+const fetchUser = require('../middleware/fetchUser');
 const JWT_SECRET = "SujeetIsgoodBoy";
 
 //ROUTE 1: Create a User using : POST"/api/auth/createUser". Doesn't require Auth
 
 router.post('/createUser', [ 
-    body('name').isLength({min: 3}),
-    body('email').isEmail(),
-    body('password').isLength({min: 5}),
+    body('name', 'Enter a valid name').isLength({min: 3}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast of length 5').isLength({min: 5}),
 ], async (req, res)=>{
     //If there are errors request Bad Request and the errors
     const errors = validationResult(req);
@@ -56,5 +56,72 @@ router.post('/createUser', [
     }
     
 })
+
+
+//ROUTE 2: Authenticate a User Using POST:"/api/auth/login", No Login Required
+
+router.post('/login', [ 
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password can\'t be null').exists()
+], async (req, res) => {
+    
+    //if there are errors, return the bad request and the error
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    try{
+        let user = await User.findOne({email});
+        
+        if(!user)
+        {
+            return res.status(400).json({error: 'Please try to login with correct credentials'});
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+
+        if(!passwordCompare)
+        {
+            return res.status(400).json({error: 'Please try to login with correct credentials'});
+        }
+
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        res.json({authToken})
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal server error occured");
+    }
+})
+
+
+//ROUTE 3: Get Loggedin User details using: POST"/api/auth/getuser", Login required
+
+router.post('/getuser', fetchUser,  async (req, res) =>{
+    
+    try{
+        userId = req.user.id;
+        const user = await User.findById(userId).select("password")
+
+        res.send({user});
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(400).send('Internal Server error');
+    }
+})
+
 
 module.exports = router
